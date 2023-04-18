6174285b11d2e122db3c3bfb4369554523edee00
waveofmymind <sjun0913@gmail.com>
2023-04-19 07:58:15 +0900

    Initial commit

---
+++ src/main/kotlin/example/springwebflux/SpringWebfluxExampleApplication.kt
@@ -0,0 +1,11 @@
+package example.springwebflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxExampleApplication>(*args)
+}
---
+++ src/test/kotlin/example/springwebflux/SpringWebfluxExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package example.springwebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebfluxExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
